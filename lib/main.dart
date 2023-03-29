91b5756773b64a5094fd1602121d81d2ba6fbfd1
shaik-adnan1 <yousufwrk0@gmail.com>
2023-03-30 01:36:13 +0530

    test commit of expences app

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Expenses App', home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expenses app"),
+      ),
+      body: Column(
+        
+      )
+    );
+  }
+}
